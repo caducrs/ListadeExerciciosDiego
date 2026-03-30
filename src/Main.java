318f749f7cdb6b46b1f53f0d44b278d03418adde
Carlos Eduardo <caduhx65@gmail.com>
2026-03-30 19:54:35 -0400

    Exercicio 3 - ArrayList e For-each

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
+
 public class Main{
 
     public static void main(String[] Args){
+
+        ArrayList<Carro> listaCarro = new ArrayList<>();
+
         Carro meuCarro = new Carro("Mercedes","Amg",2000);
+        listaCarro.add(meuCarro);
+
         Carro nossoCarro = new Carro("Toyota","Etios",2020);
+        listaCarro.add(nossoCarro);
+
+        Carro vossosCarros = new Carro("Geely","X69",2026);
+        listaCarro.add(vossosCarros);
+
+
+        for(Carro c : listaCarro){
+            c.Exibir();
+        }
 
 //        System.out.println("Carro1: " + meuCarro.marca + " " + meuCarro.modelo + " " + meuCarro.ano);
 //        System.out.println("Carro2: " + nossoCarro.marca + " " + nossoCarro.modelo + " " + nossoCarro.ano);
 
 
-        meuCarro.Exibir(); // esse metodo retorna valor
-
-        int idade = meuCarro.idadeDoCarro(); // esse metodo executa uma ação
-        System.out.println("idade do carro: " + idade);
+//        meuCarro.Exibir(); // esse metodo retorna valor
+//
+//        int idade = meuCarro.idadeDoCarro(); // esse metodo executa uma ação
+//        System.out.println("idade do carro: " + idade);
 
     }
 
