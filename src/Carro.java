62d5654aca8076eecd28c292a50e2c6d0f8a43bb
caducrs <caduhx65@gmail.com>
2026-04-05 20:42:15 -0400

    Exercício 5 – Construtor parametrizado

--- src/Carro.java
+++ src/Carro.java
@@ -1,8 +1,14 @@
 public class Carro {
     private String marca;
-    private String  modelo;
+    private String modelo;
     private int ano;
 
+    // construtor parametrizado ai o
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        setAno(ano); // reaproveita a validação
+    }
 
     public void setMarca(String marca) {
         this.marca = marca;
@@ -12,36 +18,36 @@ public class Carro {
         this.modelo = modelo;
     }
 
- public void setAno(int ano){
+    public void setAno(int ano) {
         int anoAtual = java.time.Year.now().getValue();
 
-        if(ano < 1886 || ano > anoAtual){
+        if (ano < 1886 || ano > anoAtual) {
             System.out.println("Ano Inválido! Deve estar entre 1886 e " + anoAtual);
         } else {
             this.ano = ano;
         }
- }
+    }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
-    public int getAno(){
+
+    public int getAno() {
         return ano;
     }
-    public String getModelo(){
+
+    public String getModelo() {
         return modelo;
     }
 
-    public void Exibir() {
+    public void exibir() {
         System.out.println("Marca: " + marca);
         System.out.println("Modelo: " + modelo);
         System.out.println("Ano: " + ano);
     }
-    public int idadeDoCarro(){
-        return 2026 - ano;
+
+    public int idadeDoCarro() {
+        int anoAtual = java.time.Year.now().getValue();
+        return anoAtual - ano;
     }
 }
-
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
-import java.util.ArrayList;
+public class Main {
 
-public class Main{
+    public static void main(String[] args) {
 
-    public static void main(String[] Args){
-
-        Carro c1 = new Carro();
-        c1.setMarca("Toyota");
-        c1.setModelo("Corolla");
-        c1.setAno(2023);
+        Carro c1 = new Carro("Honda", "Civic", 2022);
 
         System.out.println("Dados do carro:");
-        System.out.println("Marca: " + c1.getMarca());
-        System.out.println("Modelo: " + c1.getModelo());
-        System.out.println("Ano: " + c1.getAno());
-
+        c1.exibir();
     }
-
-
 }
