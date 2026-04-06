30813e57a45b9031b70b71ccd4c3bad5cec219cc
caducrs <caduhx65@gmail.com>
2026-04-05 21:13:57 -0400

    Exercício 13 – Passagem de objeto para método

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        Carro c1 = new Carro("Fiat", "Uno", 2020);
-        Carro c2 = c1;  // c2 recebe a referência de c1
-        c2.setModelo("Palio");
-
-        System.out.println(c1.getModelo());
-        System.out.println(c2.getModelo());
-
-        // c1 e c2 apontam para o mesmo carro na memória.
-        // Alterar o modelo por c2 também muda c1, porque ambos compartilham o mesmo objeto.
+        Carro meuCarro = new Carro("VW", "Gol", 2015);
+        trocarModelo(meuCarro);
+        System.out.println(meuCarro.getModelo());
     }
+
+    public static void trocarModelo(Carro c) {
+        c.setModelo("Fusca");
+    }
+
+    // O parametro c aponta para o mesmo objeto que meuCarro.
+    // Qualquer alteração feita por c afeta o objeto original.
 }
